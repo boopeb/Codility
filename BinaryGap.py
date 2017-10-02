38cbcd9b7baab657a1b9c164380a1d7f647d0422
Jayabalaji <Boopesh.Jayabalaji@nike.com>
2017-10-02 22:12:55 +0200

    First Code

---
+++ BinaryGap.py
@@ -0,0 +1,21 @@
+# you can write to stdout for debugging purposes, e.g.
+# print "this is a debug message"
+
+def solution(N):
+    # write your code in Python 2.7
+    aa = []
+    aa = bin(N)[2:]
+    max_gap = 0
+    current_gap = 0
+    for x in aa:
+        #print x
+        #print type(x)
+        if x == "0":
+            current_gap = current_gap + 1 
+           # print current_gap
+        else:
+            if x == "1":
+                max_gap = max(current_gap, max_gap)
+                current_gap = 0
+                
+    return max_gap
